b721aeb2326311aae8e794bc540114726a13bb1b
Jean-Philippe Lemieux <jean-philippe@macadamian.com>
2015-07-22 16:46:14 -0400

    Missing files
    Changelog:
    - new files Extras.java, PreferencesHelper.java

---
+++ src/android/Extras.java
@@ -0,0 +1,6 @@
+package com.macadamian.blinkup;
+
+public class Extras {
+    public static final String EXTRA_DEVELOPER_PLAN_ID = "Extra.DeveloperPlanId";
+    public static final String EXTRA_TIMEOUT_MS = "Extra.TimeoutMs";
+}
---
+++ src/android/PreferencesHelper.java
@@ -0,0 +1,29 @@
+package com.macadamian.blinkup;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+public class PreferencesHelper {
+    private static final String PREFERENCES_NAME = "BlinkUpPlugin";
+    private static final String PLAN_ID_KEY = "PlanId";
+
+    static private String getStringPreference(Activity activity, String key, String defVal) {
+        SharedPreferences preferences = activity.getSharedPreferences(PREFERENCES_NAME, Activity.MODE_PRIVATE);
+        return preferences.getString(key, defVal);
+    }
+
+    static private void setStringPreference(Activity activity, String key, String val) {
+        SharedPreferences preferences = activity.getSharedPreferences(PREFERENCES_NAME, Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key, val);
+        editor.apply();
+    }
+
+    static String getPlanIdKey(Activity activity) {
+        return getStringPreference(activity, PLAN_ID_KEY, null);
+    }
+
+    static void setPlanIdKey(Activity activity, String planIdKey) {
+        setStringPreference(activity, PLAN_ID_KEY, planIdKey);
+    }
+}
