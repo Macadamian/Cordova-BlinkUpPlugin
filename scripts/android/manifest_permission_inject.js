8dfd39ffd44f43df54cf2865b8300c48acfc5277
Christian Nadeau <cnadeau@macadamian.com>
2016-05-05 09:11:09 -0400

    Inject MainActivity and AndroidManifest after platform add and after plugin add. Prevent multiple MainActivity and AndroidManifest injection.

--- scripts/android/manifest_permission_inject.js
+++ scripts/android/manifest_permission_inject.js
@@ -1,29 +1,35 @@
 module.exports = function(ctx) {
   'use strict';
   // make sure android platform is part of build
-  if (ctx.opts.platforms.indexOf('android') < 0) {
+  if (ctx.opts.platforms && ctx.opts.platforms.indexOf('android') < 0) {
     return;
   }
   var fs = ctx.requireCordovaModule('fs');
   var path = ctx.requireCordovaModule('path');
   var deferral = ctx.requireCordovaModule('q').defer();
-  var jsonFile = JSON.parse(fs.readFileSync(path.join(__dirname,'manifest_permission_inject.json')));
+  var jsonFile = JSON.parse(fs.readFileSync(path.join(__dirname, 'manifest_permission_inject.json')));
   var injectPermissions = jsonFile.basePermission.concat(jsonFile.addPermissions);
 
   fs.readFile(path.join(ctx.opts.projectRoot,
-  'platforms/android/AndroidManifest.xml'), 'utf-8', function (err,data) {
+    'platforms/android/AndroidManifest.xml'), 'utf-8', function(err, data) {
     if (err) {
       return deferral.reject('Read file operation failed');
     }
-    var result = data.replace(jsonFile.basePermission,injectPermissions.join('\n\t\t'));
+
+    if (data.indexOf(jsonFile.addPermissions[0]) !== -1) {
+      console.log('CordovaBlinkUp Plugin: AndroidManifest already injected');
+      return deferral.resolve();
+    }
+
+    var result = data.replace(jsonFile.basePermission, injectPermissions.join('\n\t\t'));
 
     fs.writeFile(path.join(ctx.opts.projectRoot,
-    'platforms/android/AndroidManifest.xml'), result, 'utf-8', function (err) {
-       if (err) {
-         return deferral.reject('Write file operation failed');
-       }
-       console.log('ANDROID MANIFEST INJECTIONS ADDED SUCCESSFULLY');
-       deferral.resolve();
+      'platforms/android/AndroidManifest.xml'), result, 'utf-8', function(err) {
+      if (err) {
+        return deferral.reject('Write file operation failed');
+      }
+      console.log('CordovaBlinkUp Plugin: AndroidManifest injected successfullly');
+      deferral.resolve();
     });
   });
   return deferral.promise;
